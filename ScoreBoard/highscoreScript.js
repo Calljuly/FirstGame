23cfe521b14dff58c820a0b2a768e3420b17c3e7
Lydia Lind <lydia.nilsson@gmail.com>
2019-12-20 10:00:08 +0100

    Set up the database, you can now read and write data.

--- ScoreBoard/highscoreScript.js
+++ ScoreBoard/highscoreScript.js
@@ -1,5 +1,48 @@
 import{updateHighScore} from '../highScoreModule.js';
 
-updateHighScore();
+// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCVjVs2FiENEUVyN05mnL9ftD0B8dIFjFM",
+    authDomain: "highscorestorage.firebaseapp.com",
+    databaseURL: "https://highscorestorage.firebaseio.com",
+    projectId: "highscorestorage",
+    storageBucket: "highscorestorage.appspot.com",
+    messagingSenderId: "407629551770",
+    appId: "1:407629551770:web:b5ce7c376a3441d7e02723"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-document.getElementById('backToStart').onclick = function() {window.location.href = "game.html";}
+var database = firebase.database();
+var ref = database.ref('scores');
+
+ref.on('value', gotData, errData);
+
+function gotData(data){
+    // console.log(data.val());
+    var scores = data.val();
+    var keys = Object.keys(scores);
+    for(let i = 0; i < keys.length; i++ ){
+        var k = keys[i];
+        var name = scores[k].name;
+        var score = scores[k].score;
+        console.log(name);
+        console.log(score);
+
+    }
+}
+
+function errData(error){
+    console.log("Error!");
+    console.log(err);
+}
+
+// var data = {
+//     name: 'Lydia',
+//     score: 500,
+// }
+
+// ref.push(data);
+
+
+updateHighScore();
