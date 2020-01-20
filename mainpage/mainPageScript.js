44d92e179abfa52de9df83359cffdfbc5309f476
peterpham199 <55827530+peterpham199@users.noreply.github.com>
2020-01-20 14:41:49 +0100

    List sound
    Add sound to menu.

--- mainpage/mainPageScript.js
+++ mainpage/mainPageScript.js
@@ -1,20 +1,48 @@
 
-const audioMode = document.getElementById('audioMode');
-const audio = new Audio("/mainpage/audio/spacemusic.wav");
+// This is the sound for the whole page. Using toggle 
+const audioOn = function () {
+    const audioMode = document.getElementById('audioMode');
+    const audio = new Audio("/mainpage/audio/spacemusic.wav");
 
-audioMode.addEventListener('click', () => {
-    if (audio.paused) {
+    audioMode.addEventListener('click', () => {
+        if (audio.paused) {
 
-        audio.play();
-        document.getElementById("audioImg").src = "/mainpage/mainImages/on.png";
-    }
-    else {
+            audio.play();
+            document.getElementById("audioImg").src = "/mainpage/mainImages/on.png";
+        }
+        else {
 
-        audio.pause();
-        document.getElementById("audioImg").src = "/mainpage/mainImages/mute.png";
+            audio.pause();
+            document.getElementById("audioImg").src = "/mainpage/mainImages/mute.png";
 
 
-    }
+        }
 
-});
+    });
+
+}
+
+audioOn();
+
+
+// This is the sound for the menu
+const menuSound=function(){
+
+    const list = document.querySelectorAll('.sound');
+    const audio = new Audio("/mainpage/audio/click.wav");
+    
+
+    list.forEach(link=>{
+
+        link.addEventListener('mouseenter',()=>{
+
+            audio.play();
+        });
+
+    });
+}
+
+menuSound();
+
+
 
